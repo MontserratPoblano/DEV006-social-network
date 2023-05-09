0818102ca8c0a1516ab02501d0dc6c7f7a260ebb
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-05-09 17:48:40 -0600

    rutasGabb

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-named-as-default */
 // Este es el punto de entrada de tu aplicacion
 // import './app/signUp.js';import home from './views/home.js';
-// import home from './views/home.js';
+
 import profile from './views/profile.js';
 import home from './views/home.js';
 import signup from './views/signup.js';
