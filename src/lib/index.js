18c4483a780ecf73c9e24514287d4aea084c52d1
MontserratPoblano <christianpoblano@gmail.com>
2023-05-12 12:53:06 -0600

    cambios iniciales login

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,25 @@
 import { createUserWithEmailAndPassword, sendEmailVerification } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
 import { auth } from './firebase.js';
+import { showMessage } from './message.js';
 
 export async function registerUser(email, password) {
   try {
     const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-  } catch (error) 
+  } catch (error) {
+
+    if(error.code ==="auth/email-already-in-use"){
+      alert("Email is already in use")
+    }else if(error.code ==="auth/invalid-email"){
+      alert("Invalid email please try again")
+    }else if(error.code === "auth/weak-password"){
+      alert("Password is too weak, it should contain at least 6 characters")
+    }
+   
+  }
   
 }
-export async function validationEmail(email) {
+
+export async function validationEmail() {
   try {
     const verificationEmail = sendEmailVerification(auth.currentUser);
     console.log(verificationEmail);
--- src/views/signup.js
+++ src/views/signup.js
@@ -3,56 +3,97 @@ import { registerUser, validationEmail } from '../lib';
 function signup(navigateTo) {
   const sectionSignUp = document.createElement('section');
   const buttonReturn = document.createElement('button');
-  const formSignUp = document.createElement('form');
-  const currentCode = document.createElement('p');
-  const userSignUp = document.createElement('p');
-  const inputUserSignUp = document.createElement('input');
-  const emailSignUp = document.createElement('p');
-  const inputEmailSignUp = document.createElement('input');
-  const passwordSignUp = document.createElement('p');
-  const inputPasswordSignUp = document.createElement('input');
-  const confirmPassword = document.createElement('p');
-  const inputConfirmPasswordSignUp = document.createElement('input');
-  const buttonSignUp = document.createElement('button');
-
-  currentCode.textContent = 'current code ****';
-  inputUserSignUp.placeholder = 'Enter user';
-  inputEmailSignUp.placeholder = 'Enter email';
-  inputPasswordSignUp.placeholder = 'Enter password';
-  inputConfirmPasswordSignUp.placeholder = 'confirm password';
-  userSignUp.textContent = 'User name';
-  emailSignUp.textContent = 'E-mail';
-  passwordSignUp.textContent = 'Password';
-  confirmPassword.textContent = 'Confirm password';
-  buttonSignUp.textContent = 'SignUp';
   buttonReturn.textContent = 'Back';
   buttonReturn.classList.add('buttonReturn');
 
+  const formSignUp = document.createElement('form');
   formSignUp.classList.add('container-formSignUp');
+
+  const currentCode = document.createElement('p');
+  currentCode.textContent = 'current code ****';
   currentCode.classList.add('paragraph-signUp');
+
+  const userSignUp = document.createElement('p');
+  userSignUp.textContent = 'User name';
   userSignUp.classList.add('paragraph-signUp');
-  emailSignUp.classList.add('paragraph-signUp');
-  passwordSignUp.classList.add('paragraph-signUp');
-  confirmPassword.classList.add('paragraph-signUp');
+
+
+  const inputUserSignUp = document.createElement('input');
+  inputUserSignUp.placeholder = 'Enter user';
+  inputUserSignUp.type="text";
+  inputUserSignUp.required=true;
   inputUserSignUp.classList.add('input-userSignUp');
+
+  const emailSignUp = document.createElement('p');
+  emailSignUp.textContent = 'E-mail';
+  emailSignUp.classList.add('paragraph-signUp');
+
+  const inputEmailSignUp = document.createElement('input');
+  inputEmailSignUp.placeholder = 'Enter email';
+  inputEmailSignUp.required=true;
   inputEmailSignUp.classList.add('input-userSignUp');
+  
+
+  const passwordSignUp = document.createElement('p');
+  passwordSignUp.textContent = 'Password';
+  passwordSignUp.classList.add('paragraph-signUp');
+
+  const inputPasswordSignUp = document.createElement('input');
+  inputPasswordSignUp.placeholder = 'Enter password';
+  inputPasswordSignUp.required=true;
+  inputPasswordSignUp.type="password";
   inputPasswordSignUp.classList.add('input-userSignUp');
+  
+
+  const confirmPassword = document.createElement('p');
+  confirmPassword.textContent = 'Confirm password';
+  confirmPassword.classList.add('paragraph-signUp');
+
+
+  const inputConfirmPasswordSignUp = document.createElement('input');
+  inputConfirmPasswordSignUp.placeholder = 'confirm password';
+  inputConfirmPasswordSignUp.required=true;
+  inputConfirmPasswordSignUp.autocomplete=true;
+  inputConfirmPasswordSignUp.type="password";
   inputConfirmPasswordSignUp.classList.add('input-userSignUp');
+
+
+  const buttonSignUp = document.createElement('input');
   buttonSignUp.classList.add('btn-windowSignUp');
-
+  buttonSignUp.textContent = 'SignUp';
+  buttonSignUp.type="submit";
  
+  
 
-
-
-  buttonReturn.addEventListener('click', () => {
+   buttonReturn.addEventListener('click', () => {
     navigateTo('/');
   });
 
-  buttonSignUp.addEventListener('click', (e) => {
-    e.preventDefault();
-    registerUser(inputEmailSignUp.value, inputPasswordSignUp.value);
-    validationEmail(inputEmailSignUp.value);
+  formSignUp.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    if(inputPasswordSignUp.value===inputConfirmPasswordSignUp.value && formSignUp.checkValidity()){
+      registerUser(inputEmailSignUp.value, inputPasswordSignUp.value);
+      validationEmail(inputEmailSignUp.value);
+      // Envía el formulario a Firebase
+    }else if(inputPasswordSignUp.value !==inputConfirmPasswordSignUp.value) {
+      alert("las contraseñas no son iguales")
+    }else{
+      formSignUp.reportValidity();
+      
+    }
   });
+  
+  // buttonSignUp.addEventListener('click', () => {
+    
+  //  if(inputPasswordSignUp.value===inputConfirmPasswordSignUp.value){
+  //   registerUser(inputEmailSignUp.value, inputPasswordSignUp.value);
+  //   validationEmail(inputEmailSignUp.value);
+  //   }
+  //   else{
+  //     alert("el password no es el mismo")
+  //   }
+  // });
 
   formSignUp.append(
     currentCode,
